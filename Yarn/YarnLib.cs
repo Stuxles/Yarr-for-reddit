aff17552d6eae26fc348e7222c1a8e71443123d7
Stuxles <stuxles@live.nl>
2020-06-18 22:53:13 +0200

    apikey to private

--- Yarn/YarnLib.cs
+++ Yarn/YarnLib.cs
@@ -2,7 +2,7 @@
 {
     public class YarnLib
     {
-        public string _apiKey;
+        private string _apiKey;
         public YarnLib(string apiKey)
         {
             this._apiKey = apiKey;
